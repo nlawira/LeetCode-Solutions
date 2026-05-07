b5e5d83014422e23ca900578cf9a8383bd3b6018
Nathan Lawira <153707364+nlawira@users.noreply.github.com>
2026-05-07 23:52:25 +0800

    Added comments for Python solutions and file type for C# solution 2
    Added comments for Python solutions and fixed the file type for C# solution 2 from .txt to .cs

--- Python/1 Two Sum.py
+++ Python/1 Two Sum.py
@@ -2,6 +2,14 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numIndex = {}
 
+        # With c = target
+        # Given that a + b = c, then a = c - b
+        # If a, with its index, has been added to 'result',
+        # Then c - b verifies that a is in 'result'
+
+        # Hence, we have the index of a in 'result'
+        # And get the index of b which we found by verifying
+        # that a is in 'result'.
         for i, num in enumerate(nums):
             if target - num in numIndex:
                 return numIndex[target-num], i
--- Python/2 Add Two Numbers.py
+++ Python/2 Add Two Numbers.py
@@ -5,10 +5,12 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Instantiate the solution node and required variables.
         solNode = ListNode(0)
         currNode = solNode 
         carry = 0
 
+        # Sum up all the values in l1 and l2 into carry.
         while carry or l1 or l2:
             if l1:
                 carry += l1.val
@@ -16,8 +18,13 @@ class Solution:
             if l2:
                 carry += l2.val
                 l2 = l2.next
+            # Modulus operator because the ones place would be the sum of l1 and l2.
             currNode.next = ListNode(carry%10)
+            # The tens place, if it is a 1 because l1 + l2 >= 10, will be carried forward
+            # to the next node.
             carry //= 10
             currNode = currNode.next
-            
+
+        # We return the next node of solution node because solution node's current
+        # value is 0 when it was instantiated.    
         return solNode.next
