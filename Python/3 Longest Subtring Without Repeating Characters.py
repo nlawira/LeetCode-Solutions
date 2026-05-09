cf7f796b1b02ea64f76837cff64518897f35d977
Nathan Lawira <153707364+nlawira@users.noreply.github.com>
2026-05-09 11:58:14 +0800

    Optimized Python Solution 3
    Runtime: 7 ms, beats 99.64%
    Memory: 12.44 MB, beats 90.51%

--- Python/3 Longest Subtring Without Repeating Characters.py
+++ Python/3 Longest Subtring Without Repeating Characters.py
@@ -1,17 +1,33 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        count_list = list()
-        for i in range(0, len(s), 1):
-            counter = 1
-            for j in range(i, len(s)-1, 1):
-                if s[j+1] in s[i:j+1]:
-                    break
-                else:
-                    counter += 1
-                    continue
-            count_list.append(counter)
-        return 0 if s == "" else max(count_list)
+        last_seen = {}
+        max_len = 0
+        left = 0
+
+        # Record the most recent occurence of each 'char' in string 's' along with its
+        # most recent index into 'last_seen'.
+        # The length of the substring with no repeating characters is stored in
+        # 'substring_len'.
+        # Code will only check for the length of the next unique substring, i.e. 'substring_len'
+        # when the same character is found in 'last_seen'.
+        for right, char in enumerate(s):
+            if char in last_seen and last_seen[char] >= left:
+                # We add 1 to 'left' here because when the for loop restarts,
+                # It checks for the next index in 'right'
+                left = last_seen[char] + 1
+
+            # Input character into 'last_seen' along with its index
+            # For example, if 'a' was seen at index 2, then another 'a' is
+            # seen at index 5, it will override the previous entry.
+            # So, 'a' most recent occurence is at index 5.
+            last_seen[char] = right
+
+            # Calculate the length of the unique substring each time.
+            # If it is greater than 'max_len', override its value.
+            # We add 1 to the subtraction between 'right' and 'left'
+            # to account for the 1 added in the if statement above.
+            substring_len = right - left + 1
+            if substring_len > max_len:
+                max_len = substring_len
+
+        return max_len
